376829a18634501198df0b81338ff010999fe3de
Aman Rawat <rawataman101@gmail.com>
2023-10-29 14:06:29 +0530

    update

--- frontend/modules/adventures_page.js
+++ frontend/modules/adventures_page.js
@@ -1,39 +1,101 @@
-
 import config from "../conf/index.js";
 
 //Implementation to extract city from query params
 function getCityFromURL(search) {
   // TODO: MODULE_ADVENTURES
   // 1. Extract the city id from the URL's Query Param and return it
-
+  const urlParams = new URLSearchParams(search);
+  const city = urlParams.get("city");
+  //console.log(city);
+  return city;
 }
 
 //Implementation of fetch call with a paramterized input based on city
 async function fetchAdventures(city) {
   // TODO: MODULE_ADVENTURES
   // 1. Fetch adventures using the Backend API and return the data
-
+  try {
+    const reponse = await fetch(
+      `${config.backendEndpoint}/adventures/?city=bengaluru`
+    );
+    const adventures = await reponse.json();
+    console.log(adventures);
+    return adventures;
+  } catch (e) {
+    console.log("Error in fetch adventures");
+    return null;
+  }
 }
 
 //Implementation of DOM manipulation to add adventures for the given city from list of adventures
 function addAdventureToDOM(adventures) {
   // TODO: MODULE_ADVENTURES
   // 1. Populate the Adventure Cards and insert those details into the DOM
+  if (adventures) {
+    adventures.forEach(
+      ({ id, category, image, name, costPerHead, duration }) => {
+        // banner
+        const bannerDiv = document.createElement("div");
+        bannerDiv.className = "category-banner";
+        bannerDiv.textContent = category;
 
+        //link
+        const aDiv = document.createElement("a");
+        aDiv.setAttribute("href", `detail/?adventure=${id}`);
+        aDiv.id = id;
+
+        // rows
+        const rowDiv = document.querySelector("#data");
+
+        const colDiv = document.createElement("div");
+        colDiv.className =
+          "col-6 col-sm-6 col-md-6 col-lg-3 mb-3 position-relative";
+
+        const cardDiv = document.createElement("div");
+        cardDiv.id = id;
+        cardDiv.className = "activity-card";
+
+        const imgDiv = document.createElement("img");
+        imgDiv.src = image;
+
+        const cardBody = document.createElement("div");
+        cardBody.className = "activitycard__body w-100 p-3";
+        const cardBodyDiv1 = document.createElement("div");
+        cardBodyDiv1.className =
+          "activityCard__body1 d-flex justify-content-between";
+        cardBodyDiv1.innerHTML = `<h5>${name}</h5>
+      <p>₹${costPerHead}</p>`;
+
+        const cardBodyDiv2 = document.createElement("div");
+        cardBodyDiv2.className =
+          "activityCard__body2 d-flex justify-content-between";
+        cardBodyDiv2.innerHTML = `<h5>Duration</h5>
+      <p>${duration} Hours</p>`;
+
+        cardBody.append(cardBodyDiv1);
+        cardBody.append(cardBodyDiv2);
+
+        cardDiv.append(imgDiv);
+        cardDiv.append(cardBody);
+        aDiv.append(cardDiv);
+        colDiv.append(bannerDiv);
+        colDiv.append(aDiv);
+        rowDiv.append(colDiv);
+      }
+    );
+  }
 }
 
 //Implementation of filtering by duration which takes in a list of adventures, the lower bound and upper bound of duration and returns a filtered list of adventures.
 function filterByDuration(list, low, high) {
   // TODO: MODULE_FILTERS
   // 1. Filter adventures based on Duration and return filtered list
-
 }
 
 //Implementation of filtering by category which takes in a list of adventures, list of categories to be filtered upon and returns a filtered list of adventures.
 function filterByCategory(list, categoryList) {
   // TODO: MODULE_FILTERS
   // 1. Filter adventures based on their Category and return filtered list
-
 }
 
 // filters object looks like this filters = { duration: "", category: [] };
@@ -47,7 +109,6 @@ function filterFunction(list, filters) {
   // TODO: MODULE_FILTERS
   // 1. Handle the 3 cases detailed in the comments above and return the filtered list of adventures
   // 2. Depending on which filters are needed, invoke the filterByDuration() and/or filterByCategory() methods
-
 
   // Place holder for functionality to work in the Stubs
   return list;
@@ -66,7 +127,6 @@ function getFiltersFromLocalStorage() {
   // TODO: MODULE_FILTERS
   // 1. Get the filters from localStorage and return String read as an object
 
-
   // Place holder for functionality to work in the Stubs
   return null;
 }
@@ -78,7 +138,6 @@ function getFiltersFromLocalStorage() {
 function generateFilterPillsAndUpdateDOM(filters) {
   // TODO: MODULE_FILTERS
   // 1. Use the filters given as input, update the Duration Filter value and Generate Category Pills
-
 }
 export {
   getCityFromURL,
--- frontend/modules/landing_page.js
+++ frontend/modules/landing_page.js
@@ -10,20 +10,64 @@ async function init() {
       addCityToDOM(key.id, key.city, key.description, key.image);
     });
   }
+  console.log("1. from init() -> running @port = ", config.backendEndpoint);
 }
 
 //Implementation of fetch call
 async function fetchCities() {
   // TODO: MODULE_CITIES
   // 1. Fetch cities using the Backend API and return the data
-
+  try {
+    const response = await fetch(`${config.backendEndpoint}/cities`);
+    const cities = await response.json();
+    console.log(cities);
+    return cities;
+  } catch (e) {
+    // error in cities fetch
+    return null;
+  }
 }
 
 //Implementation of DOM manipulation to add cities
 function addCityToDOM(id, city, description, image) {
   // TODO: MODULE_CITIES
   // 1. Populate the City details and insert those details into the DOM
+  // for col
+  let rowDiv = document.querySelector("#data");
 
+  let colDiv = document.createElement("div");
+  let aDiv = document.createElement("a");
+  aDiv.setAttribute("href", `adventures/?city=${id}`);
+
+  colDiv.className = "col-xs-6 col-sm-6 col-md-6 col-lg-3";
+  aDiv.id = id;
+
+  let cardDiv = document.createElement("div");
+  cardDiv.className = "tile";
+  let img__cardDiv = document.createElement("img");
+  img__cardDiv.src = image;
+  img__cardDiv.alt = id;
+  img__cardDiv.className = "tile-img";
+  cardDiv.append(img__cardDiv);
+
+  let cardBody = document.createElement("div");
+  cardBody.className = "tile-text";
+  let city__div = document.createElement("h3");
+  city__div.innerText = city;
+  let desc__div = document.createElement("h6");
+  desc__div.innerText = description;
+
+  cardBody.appendChild(city__div);
+  cardBody.appendChild(desc__div);
+
+  cardDiv.append(cardBody);
+  colDiv.append(cardDiv);
+  colDiv.append(aDiv);
+  rowDiv.append(colDiv);
 }
+
+//addCityToDOM("london","london","london","london","london");
+//adding london city
+//addCityToDOM("london", "London", "149+ Places", "https://images.unsplash.com/photo-1535051188811-c841ac77c80b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE1fHx8ZW58MHx8fHx8&w=1000&q=80")
 
 export { init, fetchCities, addCityToDOM };
